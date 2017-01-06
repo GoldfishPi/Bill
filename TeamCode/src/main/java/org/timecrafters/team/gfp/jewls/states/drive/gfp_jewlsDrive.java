c1713e6be86fed2ef109669d43e06b5ff8b4b441
GoldfishPi <erikbadger777@gmail.com>
2017-01-05 20:01:28 -0600

    Added Drive with encoders base state

--- TeamCode/src/main/java/org/timecrafters/team/gfp/jewls/states/drive/gfp_jewlsDrive.java
+++ TeamCode/src/main/java/org/timecrafters/team/gfp/jewls/states/drive/gfp_jewlsDrive.java
@@ -1,10 +1,56 @@
 package org.timecrafters.team.gfp.jewls.states.drive;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.timecrafters.engine.Engine;
 import org.timecrafters.engine.State;
+
+import static org.firstinspires.ftc.teamcode.R.layout.motor;
 
 /**
  * Created by t420 on 1/5/2017.
  */
 
-public class gfp_jewlsDrive  {
+public class gfp_jewlsDrive extends State {
+
+    DcMotor rightFront;
+    DcMotor rightBack;
+    DcMotor leftFront;
+    DcMotor leftBack;
+
+    double speeds[] = new double[3];
+    DcMotor motors[] = new DcMotor[3];
+
+    double average;
+    double distanceSet;
+
+    @Override
+    public void exec() {
+
+        for (int i = 0; i < speeds.length; i++){
+            average += motors[0].getCurrentPosition();
+        }
+        average /= motors.length;
+
+        if(distanceSet <= average) {
+            for(int i = 0; i < motors.length;i++){
+                motors[i].setPower(speeds[i]);
+            }
+        }else{
+            for(int i = 0; i < motors.length;i++){
+                motors[i].setPower(0);
+            }
+        }
+    }
+
+    @Override
+    public void stop(){
+        for(int i = 0; i < motors.length;i++){
+            motors[i].setPower(0);
+        }
+    }
+
+    public void setDistance(double distance){
+        distanceSet = distance;
+    }
 }
